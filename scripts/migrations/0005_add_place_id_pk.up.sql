57397643a451ed0dec73e01b51eaae34f2c05983
Mattia Papaccioli <mattiapapaccioli@gmail.com>
2026-03-11 11:10:43 +0100

    fix: drop primary key with new table prefix

--- scripts/migrations/0005_add_place_id_pk.up.sql
+++ scripts/migrations/0005_add_place_id_pk.up.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-    ALTER TABLE gmaps_results DROP CONSTRAINT results_pkey;
+    ALTER TABLE gmaps_results DROP CONSTRAINT gmaps_results_pkey;
     ALTER TABLE gmaps_results ADD PRIMARY KEY (place_id);
 
 COMMIT;
